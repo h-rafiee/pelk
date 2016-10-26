7ea922a7a5e4ee1eccef841708d68c438a040223
Hossein Raffiee <h.rafiee91@gmail.com>
2016-10-26 11:09:29 +0330

    [ * add ] Fa language and set config for time and language

---
+++ local/resources/lang/fa/admin.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'header_title' => 'پنل مدیریت پلک',
+    'back_to_website' => 'بازگش به وب سایت',
+    'dashboard' => 'پیشخوان',
+    'administrators' => 'مدیران ارشد',
+    'administrator' => 'مدیر ارشد',
+    'users' => 'کاربران',
+    'user' => 'کاربر',
+    'confirm_delete' => 'آیا از حذف [ :val ] مطمئن هستید ؟',
+    'non_item_found' => 'موردی یافت نشد.',
+
+];
---
+++ local/resources/lang/fa/global.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'login' => 'ورود',
+    'logout' => 'خروج',
+    'placeholder_username' => 'نام کاربری  / ایمیل ...',
+    'placeholder_password' => 'رمز عبور ...',
+    'save' => 'ذخیره',
+    'publish' => 'انتشار',
+    'edit' => 'ویرایش',
+    'delete' => 'حذف',
+    'create_new' => 'ایجاد :name جدید',
+    'upload' => 'آپلود',
+    'empty' => 'خالی',
+    'access'=>'دسترسی',
+];
