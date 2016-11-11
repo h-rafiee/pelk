49d0301a23c63bdbf107329312c1f9ca98a8e2cb
Hossein Raffiee <h.rafiee91@gmail.com>
2016-11-11 12:00:00 +0330

    [ * add ] [ * update ] Route Api , And Passport resource and keys

--- local/routes/api.php
+++ local/routes/api.php
@@ -16,3 +16,25 @@ use Illuminate\Http\Request;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+Route::post('getToken',function(Request $request){
+    $http = new GuzzleHttp\Client;
+
+    $response = $http->post('http://localhost/pelk/oauth/token', [
+        'form_params' => [
+            'grant_type' => 'password',
+            'client_id' => $request->get('client_id'),
+            'client_secret' => $request->get('client_secret'),
+            'username' => $request->get('username'),
+            'password' => $request->get('password'),
+            'scope' => '',
+        ],
+    ]);
+
+    return json_decode((string) $response->getBody(), true);
+});
+
+Route::get('hello',function(){
+    echo "Hello Api";
+    return;
+});
