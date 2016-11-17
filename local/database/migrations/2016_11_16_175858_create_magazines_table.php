1c07be6b2ac808697d94382ba124f0b9f7428877
Hossein Raffiee <h.rafiee91@gmail.com>
2016-11-17 16:08:20 +0330

    [ * add ] migration magazine

---
+++ local/database/migrations/2016_11_16_175858_create_magazines_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMagazinesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('magazines',function(Blueprint $table){
+            $table->increments('id');
+            $table->integer('publication_id')->unsigned();
+            $table->foreign('publication_id')
+                ->references('id')->on('publications')
+                ->onDelete('cascade');
+            $table->integer('category_id')->unsigned();
+            $table->foreign('category_id')
+                ->references('id')->on('categories')
+                ->onDelete('cascade');
+            $table->string('slug')->unique();
+            $table->string('title');
+            $table->string('price')->default('0');
+            $table->string('file');
+            $table->text('description')->nullable();
+            $table->longText('text')->nullable();
+            $table->string('image')->nullable();
+            $table->text('other_images')->nullable();
+            $table->string('code')->nullable();
+            $table->string('publish_date')->nullable();
+            $table->text('params')->nullable();
+            $table->boolean('active')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('magazines');
+    }
+}
