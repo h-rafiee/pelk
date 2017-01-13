3ac6e83d47be14db471da56944aadce584011df1
Hossein Raffiee <h.rafiee91@gmail.com>
2017-01-14 00:14:20 +0330

    [ * add ] User books , magazines migrations

---
+++ local/database/migrations/2017_01_13_020333_create_user_book_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserBookTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('_user_book',function(Blueprint $table){
+
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')
+                ->references('id')->on('users');
+            $table->integer('book_id')->unsigned();
+            $table->foreign('book_id')
+                ->references('id')->on('books');
+            $table->string('key')->nullable();
+            $table->string('public')->nullable();
+            $table->string('private')->nullable();
+            $table->integer('page')->unsigned()->default(0);
+            $table->text('params')->nullable();
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('_user_book');
+    }
+}
---
+++ local/database/migrations/2017_01_13_104604_create_user_magazine.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserMagazine extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('_user_magazine',function(Blueprint $table){
+
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')
+                ->references('id')->on('users');
+            $table->integer('magazine_id')->unsigned();
+            $table->foreign('magazine_id')
+                ->references('id')->on('magazines');
+            $table->string('key')->nullable();
+            $table->string('public')->nullable();
+            $table->string('private')->nullable();
+            $table->integer('page')->unsigned()->default(0);
+            $table->text('params')->nullable();
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('_user_magazine');
+    }
+}
