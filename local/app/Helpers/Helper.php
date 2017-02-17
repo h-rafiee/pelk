16405dece8982227b52da72426128b81842e556f
Hossein Raffiee <h.rafiee91@gmail.com>
2017-02-17 11:52:15 +0330

    [ * update ] Helper encrypte files and ftp move

--- local/app/Helpers/Helper.php
+++ local/app/Helpers/Helper.php
@@ -44,4 +44,70 @@ class Helper
 
         return $success;
     }
+
+    public function move_to_ftp($file){
+        $ftp = new \FtpClient\FtpClient();
+        $ftp->connect(config('ftp.ip'));
+        $ftp->login(config('ftp.user'), config('ftp.password'));
+        $file = public_path($file);
+        $path = pathinfo($file);
+        $path['dirname'] = 'pelck/'.$path['dirname'];
+        $ftp->mkdir($path['dirname'],true);
+        if($ftp->put($path['dirname'].'/'.$path['filename'],$file,FTP_BINARY)){
+            unlink($file);
+            return TRUE;
+        }
+        return FALSE;
+    }
+
+    public function encrypt_file($file , $remove_original = FALSE){
+        $resfile = public_path($file);
+        $desfile = $resfile.".enc";
+        exec("openssl enc -aes-256-cbc -salt -in {$resfile} -out {$desfile} -pass file:/root/key.bin");
+        if($remove_original == TRUE){
+            unlink(public_path($file));
+        }
+        return $file.'.enc';
+    }
+
+    public function decrypt_file($file){
+        $resfile = public_path($file);
+        $desfile = substr($resfile,0,-4);
+        exec("openssl enc -d -aes-256-cbc -in {$resfile} -out {$desfile} -pass file:/root/key.bin");
+        return $desfile;
+    }
+
+    public function encrypt_for_user($file){
+        $resfile = public_path($file);
+        $desfile = substr($resfile,0,-4).'.plc';
+        $bytes = openssl_random_pseudo_bytes(6);
+        $key = bin2hex($bytes);
+        exec("openssl enc -d -aes-256-cbc -in {$resfile} -out {$desfile} -K $key");
+        $encryptData = $this->encrypt_key($key);
+        $data = [
+            'file'=>$desfile,
+            'original_key'=>$key,
+            'encrypt_data' => $encryptData
+        ];
+        return $data;
+    }
+
+    private function encrypt_key($key){
+        $config = array(
+            "digest_alg" => "sha512",
+            "private_key_bits" => 4096,
+            "private_key_type" => OPENSSL_KEYTYPE_RSA,
+        );
+        $res = openssl_pkey_new($config);
+        openssl_pkey_export($res, $privKey);
+        $pubKey = openssl_pkey_get_details($res);
+        $pubKey = $pubKey["key"];
+        openssl_public_encrypt($key, $encrypted, $pubKey);
+        $data= [
+            'private_key' => $privKey,
+            'public_key' => $pubKey,
+            'encrypt_key'=>$encrypted
+        ];
+        return $data;
+    }
 }
