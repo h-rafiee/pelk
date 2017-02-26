3134aba6c4b6604e2e249a169325a24f9e8e6cb2
Hossein Raffiee <h.rafiee91@gmail.com>
2017-02-27 00:15:10 +0330

    [ * test ] string of openssl

--- local/app/Helpers/Helper.php
+++ local/app/Helpers/Helper.php
@@ -102,8 +102,8 @@ class Helper
     public function encrypt_for_user($file , $key){
         $resfile = public_path($file);
         $desfile = substr($resfile,0,-4).'.plc';
-        exec("openssl enc -d -aes-256-cbc -in {$resfile} -out {$desfile} -k {$key}",$error);
-        die(var_export($error));
+        exec("openssl enc -d -aes-256-cbc -in {$resfile} -out {$desfile} -k {$key}");
+        die("openssl enc -d -aes-256-cbc -in {$resfile} -out {$desfile} -k {$key}");
         return substr($file,0,-4).'.plc';
     }
 
